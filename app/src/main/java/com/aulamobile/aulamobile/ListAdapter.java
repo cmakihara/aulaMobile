70d3f30d9a30d30a7a9e499ec7b096e492d70906
CLAUDIO MAKIHARA <CMAKIHARA2@univel.univel.br>
2018-08-14 22:03:00 -0300

    alert, save, remove.

--- app/src/main/java/com/aulamobile/aulamobile/ListAdapter.java
+++ app/src/main/java/com/aulamobile/aulamobile/ListAdapter.java
@@ -5,16 +5,20 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.List;
 
-public class ListAdapter extends RecyclerView.Adapter<ListAdapter.RecycleViewHolder>{
+public class ListAdapter extends RecyclerView.Adapter<ListAdapter.RecycleViewHolder> {
 
     private List<Tarefa> mList;
     private Context mContext;
-    public ListAdapter(Context context, List<Tarefa> list){
+    private  MainActivity activity;
+
+    public ListAdapter(Context context, List<Tarefa> list) {
         this.mContext = context;
+        this.activity = (MainActivity) context;
         this.mList = list;
     }
 
@@ -37,13 +41,22 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.RecycleViewHol
     }
 
     protected class RecycleViewHolder
-            extends RecyclerView.ViewHolder{
+            extends RecyclerView.ViewHolder {
 
         protected TextView tvTarefa;
+        protected LinearLayout llRow;
 
         public RecycleViewHolder(View itemView) {
             super(itemView);
             tvTarefa = itemView.findViewById(R.id.tv_tarefa);
+            llRow = itemView.findViewById(R.id.llRow);
+            llRow.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Tarefa tarefa = mList.get(getAdapterPosition());
+                    activity.onClickList(tarefa);
+                }
+            });
         }
     }
 }
--- app/src/main/java/com/aulamobile/aulamobile/MainActivity.java
+++ app/src/main/java/com/aulamobile/aulamobile/MainActivity.java
@@ -1,12 +1,15 @@
 package com.aulamobile.aulamobile;
 
+import android.content.DialogInterface;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
@@ -17,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
 
     private FloatingActionButton fabAdd;
     private RecyclerView rvList;
+    private LinkedList<Tarefa> list;
+    private ListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +37,13 @@ public class MainActivity extends AppCompatActivity {
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         this.rvList.setLayoutManager(linearLayoutManager);
 
-        List<Tarefa> list = new LinkedList<>();
+        
+        list = new LinkedList<>();
         for (int i = 0; i < 30; i++){
             list.add(new Tarefa("Tarefa " + i));
         }
 
-        ListAdapter adapter = new ListAdapter(this, list);
+        adapter = new ListAdapter(this, list);
         rvList.setAdapter(adapter);
         adapter.notifyDataSetChanged();
 
@@ -47,13 +53,43 @@ public class MainActivity extends AppCompatActivity {
         fabAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"Clicou", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(getApplicationContext(),"Clicou", Toast.LENGTH_SHORT).show();
+                addTask();
+
             }
         });
     }
 
+    private void addTask() {
+        final EditText etTask = new EditText(this);
+        AlertDialog dialog = new AlertDialog.Builder(this)
+                .setTitle("Tarefa")
+                .setMessage("Add Tarefa")
+                .setView(etTask)
+                .setPositiveButton("Salvar", new DialogInterface.OnClickListener() {
+                    @Override
+
+                    public void onClick(DialogInterface dialog, int i) {
+                        Tarefa tarefa = new Tarefa(etTask.getText().toString());
+                        list.add(tarefa);
+                        adapter.notifyDataSetChanged();
+                        Toast.makeText(getApplicationContext(), "Salvou", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("Cancelar", null). create();
+        dialog.show();
+
+    }
+
+
     private void getElements() {
         this.rvList = findViewById(R.id.rvList);
         this.fabAdd = findViewById(R.id.fabAdd);
+    }
+
+    public void onClickList(Tarefa tarefa) {
+        Toast.makeText(getApplicationContext(), tarefa.getTarefa(), Toast.LENGTH_SHORT).show();
+        list.remove(tarefa);
+        adapter.notifyDataSetChanged();
     }
 }
